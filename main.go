436b9694304a0827df0f83cbaf1fa2295625a210
luke-taylor <lukejamestaylor98@outlook.com>
2024-08-03 19:31:04 +0000

    add configure func

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-    "context"
-    "log"
+	"context"
+	"log"
 
-    "terraform-provider-azureipam/internal/provider"
+	"terraform-provider-azureipam/internal/provider"
 
-    "github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+)
+
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary.
+	version string = "dev"
+
+	// goreleaser can pass other information to the main package, such as the specific commit
+	// https://goreleaser.com/cookbooks/using-main.version/
 )
 
 func main() {
-    opts := providerserver.ServeOpts{
-        Address: "hashicorp.com/edu/azureipam",
-    }
+	opts := providerserver.ServeOpts{
+		Address: "hashicorp.com/edu/azureipam",
+	}
 
-    err := providerserver.Serve(context.Background(), provider.New(), opts)
-    if err != nil {
-        log.Fatal(err.Error())
-    }
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
